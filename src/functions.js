19351cf525421a4364a662d8db4faaff2fe20d7f
Sfack <you@example.com>
2025-03-08 22:55:43 +0200

    almost done with dropdowns

--- src/functions.js
+++ src/functions.js
@@ -1,14 +1,13 @@
-function displayMenu(type) {
-    const menuContainer = $('#menu');
+function displayMenu(type, menu) {
+    menuContainer = $(menu);
     menuContainer.empty();
+
 
     const itemTypes = {};
 
 
 
     menuData[type].forEach(item => {
-        
-     
 
             if(!itemTypes[item.type]) 
                  itemTypes[item.type] = [];
@@ -16,13 +15,8 @@ function displayMenu(type) {
             if (!itemTypes[item.type][item.category]) 
                     itemTypes[item.type][item.category] = [];
                 
-            itemTypes[item.type][item.category].push(item);
-                
- 
+            itemTypes[item.type][item.category].push(item); 
     });
-
-
-
 
     Object.keys(itemTypes).forEach(type => {
         const typeLabel = $('<h2></h2>').text(type);
@@ -44,12 +38,30 @@ function displayMenu(type) {
     
 }
 
-function filterMenu(category, button) {
-    $('.filter-btn').removeClass('selected');
-    $(button).addClass("selected");
-    displayMenu(category);
-}
+// function filterMenu(category, button) {
+//     $('.filter-btn').removeClass('selected');
+//     $(button).addClass("selected");
+//     displayMenu(category);
+// }
 
-$(document).ready(() => {
-    displayMenu('drinks');
-});
+// $(document).ready(() => {
+//     displayMenu('drinks');
+// });
+
+
+function dropMenu(menuDropdown, category) {
+    console.log(menuDropdown);
+
+    let menu = menuDropdown.querySelector('.menu');
+    let arrow = menuDropdown.querySelector('.menu-downarrow');
+
+    if ($(menuDropdown).height() === 500) {
+        $(menuDropdown).animate({ height: "200px" }, 300); 
+        $(arrow).css('transform', 'rotate(0deg)');
+
+    } else {
+        $(menuDropdown).animate({ height: "500px" }, 300); 
+        displayMenu(category, menu); 
+        $(arrow).css('transform', 'rotate(180deg)');
+    }
+}
