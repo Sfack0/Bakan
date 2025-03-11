5ddd2a69ff2b0ffe452e1633939349ab87f74353
Sfack <you@example.com>
2025-03-11 14:17:07 +0200

    testing

--- src/product.js
+++ src/product.js
@@ -6,7 +6,7 @@ const menuData ={
             "name": "Espresso",
             "type": "Coffee",
             "category": "hot",
-            "price": "1.80/2.40/2.90",
+            "price": "1.80/2.40/2.93",
             "img" : "../images/Coffee.png"
         },
         {
